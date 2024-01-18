e84a9ce4ea5a16b05bd0936ec064b6e1b799c938
aminovmunir <aminov.munirjon.0412@gmail.com>
2024-01-18 10:19:48 +0500

    easy/26

---
+++ easy/26.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ */
+var removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == nums[i + 1]) {
+      nums[i] = 101;
+    }
+  }
+  nums.sort((a, b) => a - b);
+  return new Set(nums).size - 1;
+};
+
+console.log(removeDuplicates([1, 1, 2]));
